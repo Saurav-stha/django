6a41864ec9074ab672834fbf52d867f68ff2e8ba
Saurav-sth <roronoasulav@gmail.com>
2023-06-17 19:31:24 +0545

    model_changed

--- hells/home/models.py
+++ hells/home/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name=models.CharField(max_length=50,default='')
-    email=models.CharField(max_length=50)
+    name=models.CharField(max_length=128)
+    email=models.CharField(max_length=128)
     desc=models.TextField()
     date = models.DateField()
 
